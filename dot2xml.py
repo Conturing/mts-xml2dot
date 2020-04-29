5484a38067a81ac8c34c519988d94485ade614ff
Conturing <13372078+Conturing@users.noreply.github.com>
2020-04-29 10:21:02 +0200

    Refactoring.

---
+++ dot2xml.py
@@ -0,0 +1,80 @@
+from mts import MTS
+
+def mts_to_dot(path, mts: MTS):
+    class_text = 'digraph'
+
+    states = []
+    state_mapping = {}
+    uid = 0
+    for state in mts.states:
+        state_mapping[state['id']] = uid
+        states.append(
+            's{id} [shape="circle" label="{label}"];'.format(id=uid, label=state['id'])
+        )
+        uid += 1
+
+    transitions = []
+    for transition in mts.transitions:
+        transitions.append(
+            's{src} -> s{dest} [modality="{mod}", style="{style}", label="{label}"];'.format(
+                src=state_mapping[transition["src"]],
+                dest=state_mapping[transition["dest"]],
+                mod="MUST" if transition["must"] else "MAY",
+                style="dashed",
+                label=transition["label"]
+            )
+        )
+
+    with open(path, 'wt') as f:
+        f.write(f'{class_text} g {{\n\n')
+        for s in states:
+            f.write('\t' + s + '\n')
+        f.write('\n')
+        for t in transitions:
+            f.write('\t' + t + '\n')
+        f.write('\n')
+        f.write('__start0 [label="" shape="none" width="0" height="0"];\n')
+        f.write('__start0 -> s0;\n')
+        f.write('}\n')
+
+
+def mc_to_dot(path, mts: MTS):
+    class_text = 'digraph'
+
+    states = []
+    state_mapping = {}
+    uid = 0
+    for state in mts.states:
+        state_mapping[state['id']] = uid
+        states.append(
+            's{id} [shape="circle" label="{label}"];'.format(id=uid, label=state['id'])
+        )
+        uid += 1
+
+    transitions = []
+    for transition in mts.transitions:
+        transitions.append(
+            's{src} -> s{dest} [modality="{mod}", style="{style}", {color} contract="{contract}", label="{label}"];'.format(
+                src=state_mapping[transition["src"]],
+                dest=state_mapping[transition["dest"]],
+                mod="MUST" if transition["must"] else "MAY",
+                style="dashed",
+                label=transition["label"],
+                color='color="green"' if transition["green"] else
+                ('color="red"' if transition["red"] else ''),
+                contract='GREEN' if transition["green"] else
+                ('RED' if transition["red"] else 'NONE')
+            )
+        )
+
+    with open(path, 'wt') as f:
+        f.write(f'{class_text} g {{\n\n')
+        for s in states:
+            f.write('\t' + s + '\n')
+        f.write('\n')
+        for t in transitions:
+            f.write('\t' + t + '\n')
+        f.write('\n')
+        f.write('__start0 [label="" shape="none" width="0" height="0"];\n')
+        f.write('__start0 -> s0;\n')
+        f.write('}\n')
---
+++ parse_xml.py
@@ -0,0 +1,42 @@
+from distutils.util import strtobool
+from xml.etree import ElementTree as ET
+import logging
+
+from mts import MTS
+
+log = logging.getLogger(__name__)
+
+def parse_xml(path) -> MTS:
+    tree = ET.parse(path)
+    root = tree.getroot()
+
+    mts = MTS()
+
+    states = root.find('states')
+    transitions = root.find('transitions')
+
+    for state in states:
+        if state.tag != 'state':
+            raise ValueError('Expected tag \'state\'')
+        id = state.find('id').text
+        for attr in state.find('attributes'):
+            if attr.tag != 'isInitial':
+                log.warning(f'Tag \'{attr.tag}\' is unknown')
+            else:
+                initial = bool(strtobool(attr.text))
+        mts.add_state(id=id, initial=initial)
+
+    for transition in transitions:
+        if transition.tag != 'transition':
+            raise ValueError('Expected tag \'transition\'')
+        src   = transition.find('sourceId').text
+        dest  = transition.find('targetId').text
+        label = transition.find('label').text
+        attr  = transition.find('attributes')
+        may   = bool(strtobool(attr.find('isMay').text))
+        must  = bool(strtobool(attr.find('isMust').text))
+        green = bool(strtobool(attr.find('isGreen').text))
+        red   = bool(strtobool(attr.find('isRed').text))
+        mts.add_transition(src, dest, label, may, must, green, red)
+
+    return mts
